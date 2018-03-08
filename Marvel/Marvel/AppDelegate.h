2560610e0dce116afaeba663d1c44215e425282b
mabiao5 <mabiao5@wanda.cn>
2018-03-08 15:55:42 +0800

    porject init

---
+++ Marvel/Marvel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marvel
+//
+//  Created by BiaoShu on 2018/3/8.
+//  Copyright © 2018年 BiaoShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Marvel/Marvel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marvel
+//
+//  Created by BiaoShu on 2018/3/8.
+//  Copyright © 2018年 BiaoShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
